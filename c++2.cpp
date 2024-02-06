37b450e9415b27dc76e21a60cc0a638c63fbb219
Sabrinasssssss <129230514+Sabrinasssssss@users.noreply.github.com>
2024-02-06 12:11:12 +0300

    Test

---
+++ c++2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int score = 0;
+
+  cout << "Вопрос 1: Какая столица у Франции?' << endl;
+    cout << "a) Лондон" << endl;
+    cout << "b) Париж" << endl;
+    cout << "c) Берлин" << endl;
+    cout << "d) Рим" << endl;
+char answer1;
+cout << "Ваш ответ: ";
+cin >> answer1;
+if (answer == 'b') {
+  score++;
+}
+
+//Другие вопросы и ответы...
+
+cout << "Вы набрали " << score << " из 5 баллов" <<endl;
+return 0;
+}
